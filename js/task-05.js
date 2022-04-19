e36562d6b88b0b5f28e0c1f127c867184f0624cf
Kostyantyn Krivka <koskvk@gmail.com>
2022-04-19 23:41:12 +0300

    remove coments

--- js/task-05.js
+++ js/task-05.js
@@ -1,18 +1,3 @@
-// const refs = {
-//     input: document.querySelector('#name-input'),
-//     output: document.querySelector('#name-output'),
-// };
-
-// refs.input.addEventListener('input', chengeOutput);
-
-// function chengeOutput(event) {
-//     const inputEl = event.currentTarget.value;
-//     if (inputEl) {
-//         return refs.output.textContent = inputEl;
-//     };
-//     return refs.output.textContent = 'Anonymous';
-// };
-// ---------------------------------------------------------------------------------
 const refs = {
     input: document.querySelector('#name-input'),
     output: document.querySelector('#name-output'),
